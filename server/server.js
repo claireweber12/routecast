5770a2f3782111c46f8deaccdc2e7e2f3eda7cc2
claireweber12 <claire.weber2004@icloud.com>
2026-05-04 11:20:47 -0500

    added latitude and longitude based mapping

--- server/server.js
+++ server/server.js
@@ -16,6 +16,7 @@ app.get("/", (req, res) => {
 
 app.get("/api/weather",async (req, res) => {
     const city = req.query.city;
+    const state = req.query.state || "";
 
     if(!city) {
         return res.status(400).json({error: "City is required"});
@@ -23,24 +24,38 @@ app.get("/api/weather",async (req, res) => {
     }
     const apiKey = process.env.OPENWEATHER_API_KEY;
     try{
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(
-      city
-    )}&appid=${apiKey}&units=imperial`;
+      const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},US&limit=1&appid=${apiKey}`;
+      const geoResponse = await fetch(geoUrl);
+      const geoData=await geoResponse.json();
+      if (!geoResponse.ok || geoData.length === 0) {
+        return res.status(404).json({
+        error: "Location not found.",
+        });
+      }
+      const lat = geoData[0].lat;
+      const lon = geoData[0].lon;
 
-    const response = await fetch(url);
-    const data = await response.json();
-    if (!response.ok) {
-      return res.status(response.status).json({
-        error: data.message || "Could not fetch weather",
+
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+    
+    const weatherResponse = await fetch(weatherUrl);
+    const weatherData = await weatherResponse.json();
+    if (!weatherResponse.ok) {
+      return res.status(weatherResponse.status).json({
+        error: weatherData.message || "Could not fetch weather",
       });
     }
     res.json({
-        city:data.name,
-        country:data.sys.country,
-        temperature:data.main.temp,
-        feelsLike:data.main.feels_like,
-        condition:data.weather[0].description,
-        windSpeed: data.wind.speed,
+        city: weatherData.name,
+        state:geoData.state,
+        country: geoData[0].country,
+        lat,
+        lon,
+        description: weatherData.weather[0].description,
+        temperature: weatherData.main.temp,
+        feelsLike: weatherData.main.feels_like,
+        humidity: weatherData.main.humidity,
+        windSpeed: weatherData.wind.speed,
     });
     } catch (error){
         console.error("Weather route error", error);
